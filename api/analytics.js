de145a0955093a492e846ed1faf8fd3416662428
abogutskaya22-maker <a.bogutskaya22@gmail.com>
2026-09-12 12:37:36 +0300

    Allow public request submission endpoint

--- api/analytics.js
+++ api/analytics.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(204).end();
+
   const token = process.env.TELEGRAM_BOT_TOKEN;
   const chatId = process.env.TELEGRAM_CHAT_ID;
 
